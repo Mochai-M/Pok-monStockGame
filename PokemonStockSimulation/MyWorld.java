dd1e7253140d6edb7e8cf03102df117f824d5918
natalieh2 <150183401+natalieh2@users.noreply.github.com>
2023-11-14 13:46:28 -0500

    added in icons class

--- PokemonStockSimulation/MyWorld.java
+++ PokemonStockSimulation/MyWorld.java
@@ -3,13 +3,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author Mekaeel Malik
+ * @author Mekaeel Malik, Natalie Huang
  * @version 1.0
  */
 public class MyWorld extends World
 {
+<<<<<<< Updated upstream
 
     // natalie 11/7/2023
+=======
+    //load images into a temporary array
+    //GreenfootImage[] images = new GreenfootImage[];
+    //new GreenfootImage("*add image*");
+    //do four times for each emotion
+    //icon will then be equal to new icon(images);
+    
+    //private GreenfootImage[] images; 
+    GreenfootImage icon1 = new GreenfootImage(
+>>>>>>> Stashed changes
     
     /**
      * Constructor for objects of class MyWorld.
@@ -17,6 +28,7 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
+<<<<<<< Updated upstream
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1600, 800, 1,false);
         
@@ -26,5 +38,10 @@ public class MyWorld extends World
         
         addObject(new FireCompany(400), 1625,300);
         addObject(new WaterCompany(400), 1625,300);
+=======
+        super(1100, 650, 1); 
+        
+>>>>>>> Stashed changes
     }
+    
 }
