8eb9e1a8ddd3b7e259b81fb4681098fb8a0d2eb7
liyuxiao222 <89355060+liyuxiao2@users.noreply.github.com>
2023-11-24 14:41:31 -0500

    finished weather class/images

--- PokemonStockSimulation/GreenEffect.java
+++ PokemonStockSimulation/GreenEffect.java
@@ -5,12 +5,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * 
  * @Liyu
  * @Nov 11 2023
+ * 
+ * image link: https://tenor.com/view/leaves-gif-16219912
  */
 public class GreenEffect extends Weather
 {
     public GreenEffect(){
         //sets the size and length for the effect
-        super(300,60, "leaves.gif");
+        super(300,100, "leaves.gif");
         
         
     }
--- PokemonStockSimulation/RedEffect.java
+++ PokemonStockSimulation/RedEffect.java
@@ -4,19 +4,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * The effect for the Red stock
  * 
  * @Liyu 
- * @Nov 11 2023
+ * @Nov 24 2023
+ * 
+ * 
+ * sound Effects credits link: https://elements.envato.com/fire-BD6EJQS?utm_source=mixkit&utm_medium=referral&utm_campaign=elements_mixkit_cs_sfx_tag_CM2023&_ga=2.87020696.1834148410.1700853703-1213142309.1698190566
+ * Image Credits link : https://martincrownover.com/gamemaker-examples-tutorials/particles-fire/
  */
 public class RedEffect extends Weather
 {
+    //initialize, the fire sound
+    GreenfootSound fireSound = new GreenfootSound("fire.mp3");
+    
+    
     public RedEffect(){
         //sets the size and length for the effect
-        super(300,60);
-
+        super(300,120, "FireEffect.gif");
+        
+        fireSound.setVolume(30);
+        
     }
     
     public void act()
     {
-        
+        fireSound.play();
         super.act("FireCompany", "GreenCompany");
     }
 }
